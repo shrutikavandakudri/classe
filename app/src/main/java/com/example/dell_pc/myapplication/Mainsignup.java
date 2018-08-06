4998a2f3753f6254a2ee85f4138a974f2661a81c
shrutika <shrutikavandakudri@gmail.com>
2018-08-06 16:31:35 +0530

    completed student part

--- app/src/main/java/com/example/dell_pc/myapplication/Mainsignup.java
+++ app/src/main/java/com/example/dell_pc/myapplication/Mainsignup.java
@@ -1,13 +1,47 @@
 package com.example.dell_pc.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Mainsignup extends AppCompatActivity {
+     RadioGroup radiobutton;
+     EditText  signupname,signuppass;
+     Button nextstepbutton;
+    FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainsignup);
+
+        signupname = (EditText) findViewById(R.id.signupname);
+        signuppass = (EditText) findViewById(R.id.signuppass);
+        radiobutton = (RadioGroup) findViewById(R.id.radioid);
+        nextstepbutton = (Button) findViewById(R.id.nextbutton);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+
+        nextstepbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Mainsignupnext.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/dell_pc/myapplication/Studentpage.java
+++ app/src/main/java/com/example/dell_pc/myapplication/Studentpage.java
@@ -1,13 +1,46 @@
 package com.example.dell_pc.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
 
 public class Studentpage extends AppCompatActivity {
+    FrameLayout studentframe;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studentpage);
+        FrameLayout settingImage=(FrameLayout) findViewById(R.id.studframe);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+
+        studentframe.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Studassignment.class));
+            }
+
+        });
+
     }
 }
